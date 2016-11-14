09880006dab4b05e1c43fc9d874ad34a1e6e6cd3
Doulala <sodoulala@163.com>
2016-11-14 17:37:49 +0800

    sleuth zipkin stream

--- Zipkin/src/main/java/com/ctsi/cloud/zipkin/ZipkinApplication.java
+++ Zipkin/src/main/java/com/ctsi/cloud/zipkin/ZipkinApplication.java
@@ -3,7 +3,6 @@ package com.ctsi.cloud.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinStreamServer
